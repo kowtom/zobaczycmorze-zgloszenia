d8ca9c37728e4a716248c1e76f3048e240d214a0
TotalZero <radek.solekr@icloud.com>
2025-12-05 20:45:48 +0100

    zmiana w funkcji send_simple_mail, dodanie nowych szablonow email, zgodnych ze stara platforma.

--- rejs/mailers.py
+++ rejs/mailers.py
@@ -1,21 +1,29 @@
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.conf import settings
 
 FROM = getattr(settings, "DEFAULT_FROM_EMAIL", "noreply@zobaczyc.morze")
 
 def send_simple_mail(subject, to_mail, template_base, context):
-	TXT = render_to_string(template_base + ".txt", context)
+	"""
+	wysyla emaila w formacie html i txt jako fallback.
+	wymaga template_base.html i/lub template_base.txt
+	"""
+	try:
+		TXT = render_to_string(template_base + ".txt", context)
+	except Exception:
+		TXT = None
 	try:
 		HTML = render_to_string(template_base + ".html", context)
 	except Exception:
 		HTML = None
 
-	send_mail(
+	email = EmailMultiAlternatives(
 		subject=subject,
-		message=TXT,
+		body=TXT,
 		from_email=FROM,
-		recipient_list=[to_mail],
-		fail_silently=False,
-		html_message=HTML
+		to=[to_mail],
 	)
+	if HTML:
+		email.attach_alternative(HTML, "text/html")
+	email.send(fail_silently=False)
